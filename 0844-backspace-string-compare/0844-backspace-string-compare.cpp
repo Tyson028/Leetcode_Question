c8bd673db123698d47340521b4e28224e24c05b8
Shashi Ranjan <shashi.ranjan3352@gmail.com>
2026-08-04 00:50:53 +0530

    Time: 0 ms (100%), Space: 8.4 MB (86.96%) - LeetHub

--- 0844-backspace-string-compare/0844-backspace-string-compare.cpp
+++ 0844-backspace-string-compare/0844-backspace-string-compare.cpp
@@ -1,39 +1,54 @@
 class Solution {
 public:
     bool backspaceCompare(string s, string t) {
-        int n=s.size();
-        stack<char> st1;
-        for(int i=0;i<n;i++){
-            char ch1=s[i];
-            if(ch1!='#')
-                st1.push(ch1);
-            else if(!st1.empty())
-                st1.pop();
-        }
-        string str1="";
-        while(!st1.empty()){
-            str1+=st1.top();
-            st1.pop();
-        }
-        reverse(str1.begin(),str1.end());
 
-        int m=t.size();
-        stack<char> st2;
-        for(int i=0;i<m;i++){
-            char ch2=t[i];
-            if(ch2!='#')
-                st2.push(ch2);
-            else if(!st2.empty())
-                st2.pop();
-        }
-        string str2="";
-        while(!st2.empty()){
-            str2+=st2.top();
-            st2.pop();
-        }
-        reverse(str2.begin(),str2.end());
+        int i=s.size()-1;
+        int j=t.size()-1;
+        int skipS=0,skipt=0;
 
-        if(str1==str2) return true;
-        else return false;
+        while(i>= 0 || j>=0){
+
+            while(i>=0){
+
+                if(s[i]=='#'){
+                    skipS++;
+                    i--;
+                }
+                
+                else if(skipS>0){
+                    skipS--;
+                    i--;
+                }
+                else{
+                    break;
+                }
+            }
+
+            while(j>=0){
+                if(t[j]=='#'){
+                    skipt++;
+                    j--;
+                }
+                else if(skipt>0){
+                    skipt--;
+                    j--;
+                }
+                else{
+                    break;
+                }
+            }
+
+            if(i>= 0 && j>=0 ){
+                if(s[i] != t[j]) 
+                    return false;
+            }
+            else{
+                if(i>=0 || j>=0) 
+                    return false;
+            }
+
+            i--,j--;
+        }
+        return true;
     }
 };
